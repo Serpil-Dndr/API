19c8de642de761036c227ab77a88d8262f1cc08f
Serpil-Dndr <serpil.dndrca@gmail.com>
2023-08-03 23:46:59 -0400

    added day4

--- src/test/java/day04/C01_GetRequest.java
+++ src/test/java/day04/C01_GetRequest.java
@@ -1,7 +1,120 @@
 package day04;
 
-public class C01_GetRequest {
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.w3c.dom.CDATASection;
+import utilities.BaseUrlSpec;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+
+
+public class C01_GetRequest extends BaseUrlSpec {
+
+
+        @Test
+        public void getReqresAll () {
+
+        /*
+         *** TC07:Send a GET Request to https://reqres.in/api/users
+              and verify:
+              The status code is 200
+              The content type is application/json; charset=utf-8
+              Verify that the following exists in data
+              email is tracey.ramos@reqres.in
+              first_name is Tracey
+              last_name is Ramos
+              avatar is https://reqres.in/img/faces/6-image.jpg
+
+             {
+                "id": 6,
+                "email": "tracey.ramos@reqres.in",
+                "first_name": "Tracey",
+                "last_name": "Ramos",
+                "avatar": "https://reqres.in/img/faces/6-image.jpg"
+             }
+
+         */
+
+            // 1- End-point and request body
+
+            specReqresIn.pathParams("pp1", "api", "pp2", "users");
+
+            // 2- Expected data
+
+            // 3- Send the request and save the response
+
+            Response response = given().spec(specReqresIn)
+                    .when()
+                    .get("/{pp1}/{pp2}");
+
+
+            // 4- Assertions
+
+            response
+                    .then()
+                    .statusCode(200)
+                    .contentType("application/json; charset=utf-8")
+                    .body("data[5].id",Matchers.equalTo(6),
+                      "data[5].first_name",Matchers.equalTo("Tracey"),
+                            "data[5].last_name",Matchers.equalTo("Ramos"),
+                            "data[5].avatar",Matchers.equalTo("https://reqres.in/img/faces/6-image.jpg"));
+
+
+        }
+
+
+        @Test
+        public void getBooking () {
+        /*
+        *** TC08:Send a GET Request to https://restful-booker.herokuapp.com/booking/433
+     and verify:
+     The status code is 200
+     The content type is application/json; Charset=utf-8
+     firstname is John
+     lastname is Smith
+     totalprice is 111
+     depositpaid is true
+     bookingdates (checkin) is 2018-01-01
+     bookingdates (checout) is 2019-01-01
+
+        *  {
+       "firstname": "Jane",
+       "lastname": "Smith",
+       "totalprice": 111,
+       "depositpaid": true,
+       "bookingdates": {
+           "checkin": "2018-01-01",
+           "checkout": "2019-01-01"
+       },
+       "additionalneeds": "Extra pillows please"
+   }
+
+
+         */
+            //1. end point
+
+
+  specHerokuApp.pathParams("pp1","booking","pp2","100");
+  //
+            Response response = given().spec(specHerokuApp)
+                    .when()
+                    .get("/{pp1}/{pp2}");
+            //Assertion
+            response
+                    .then()
+                    .statusCode(200)
+                    .contentType("application/json; Charset=utf-8")
+                    .body("firstname",Matchers.equalTo("John"),
+                            "lastname",Matchers.equalTo("Smith"),
+                            "depositpaid",Matchers.equalTo(true),
+                            "bookingdates.checkin",Matchers.equalTo("2018-01-01"),
+                            "bookingdates.checkout",Matchers.equalTo("2019-01-01"));
 
 
 
-}
+
+        }
+
+    }
+
---
+++ src/test/java/day04/C02_GetRequest.java
@@ -0,0 +1,115 @@
+package day04;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+import utilities.BaseUrlSpec;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class C02_GetRequest extends BaseUrlSpec {
+
+    @Test
+    public void userId1RegressIn(){
+/*
+*** TC08:Send a GET Request to https://reqres.in/api/users/1
+             and verify:
+             The status code is 200
+             The content type is application/json; Charset=utf-8
+             email is george.bluth@reqres.in
+             first_name is George
+             last_name is Bluth
+             avatar is https://reqres.in/img/faces/1-image.jpg
+         {
+           "id": 1,
+           "email": "george.bluth@reqres.in",
+           "first_name": "George",
+           "last_name": "Bluth",
+           "avatar": "https://reqres.in/img/faces/1-image.jpg"
+         },
+         *
+
+ */
+        // end-point and request
+        specReqresIn.pathParams("pp1","api","pp2","users");
+        //2-expected data
+        //3- send request and save response
+
+        Response response = given().spec(specReqresIn)
+                .when()
+                .get("/{pp1}/{pp2}");
+        //Assertion
+//        response
+//                .then()
+//                .statusCode(200)
+//                .contentType("application/json; Charset=utf-8");
+        JsonPath jsonPath = response.jsonPath();
+        System.out.println(jsonPath.getList("data.email"));
+        System.out.println(jsonPath.getList("data.first_name"));
+        System.out.println(jsonPath.getList("data.last_name"));
+
+        assertEquals(1,jsonPath.getInt("data[0].id"));
+        assertEquals("george.bluth@reqres.in",jsonPath.getString("data[0].email"));
+        assertEquals("George",jsonPath.getString("data[0].first_name"));
+        assertEquals("Bluth",jsonPath.getString("data[0].last_name"));
+        assertEquals("https://reqres.in/img/faces/1-image.jpg",jsonPath.getString("data[0].avatar"));
+
+
+    }
+    @Test
+    public void postHerokuapp(){
+
+/*
+{
+            "firstname" : "Ahmet",
+            "lastname" : “Bulut",
+            "totalprice" : 500,
+            "depositpaid" : false,
+            "bookingdates" : {
+            "checkin" : "2023-01-10",
+            "checkout" : "2023-01-20"
+                                        },
+            "additionalneeds" : "wi-fi"
+           }
+           Validate for response
+        status code 200
+        content type application/json
+        "firstname" "Ahmet"
+        "lastname" "Bulut"
+        "totalprice" 500
+        "depositpaid" false
+        "checkin"  "2023-01-10",
+         "checkout"  "2023-01-20"
+        "additionalneeds" wi-fi
+ */
+        //1- end point
+      //  String url ="https://restful-booker.herokuapp.com/booking";
+       specHerokuApp.pathParams("pp1","booking");
+        JSONObject requestBody = new JSONObject();
+        JSONObject bookingDates = new JSONObject();
+        bookingDates.put("checkin","2023-01-10");
+        bookingDates.put("checkout","2023-01-20");
+        requestBody.put("firstname","Ahmet");
+        requestBody.put("lastname","Bulut");
+        requestBody.put("totalprice",500);
+        requestBody.put("depositpaid","true");
+        requestBody.put("bookingdates",bookingDates);
+        requestBody.put("addintinalneeds","wi-fi");
+        Response response = given().spec(specHerokuApp)
+                .when().body(requestBody.toString())
+                .post("/{pp1}");
+        response.prettyPrint();
+
+//  response.then()
+//          .assertThat()
+//          .statusCode(200)
+//          .contentType("application/json")
+//          .body();
+
+
+    }
+}
